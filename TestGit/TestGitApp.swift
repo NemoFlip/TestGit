2fe27fc201a843d9ba7fe4ab4799343bfae9854e
NemoFlip <artgamer3163@gmail.com>
2023-08-04 15:37:40 +0300

    Initial Commit

---
+++ TestGit/TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitApp.swift
+//  TestGit
+//
+//  Created by Artem on 04.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
